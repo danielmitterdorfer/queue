88805f35ce7ae520c1313e93575045f6ec54c9e5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2022-11-29 10:50:48 +0100

    Run a mixed benchmark workload
    Originally we have used `rand.Int()` to determine whether to enqueue or
    dequeue and assumed that it would return both negative and positive
    numbers whereas in reality it returns only non-negative numbers. With
    this commit we use `rand.IntN(n int)` instead to control the range and
    enqueue/dequeue with a 50% chance. We also avoid overcommitting Go
    routines and only use the number of available CPUs as this benchmark is
    CPU bound anyway. The number of assigned CPUs should be controlled via
    the parameter `-cpu` on the command line.

--- bench_queue_test.go
+++ bench_queue_test.go
@@ -2,8 +2,6 @@ package queue
 
 import (
 	"math/rand"
-	"runtime"
-	"strconv"
 	"sync/atomic"
 	"testing"
 )
@@ -18,28 +16,25 @@ func BenchmarkQueue(b *testing.B) {
 	length := 1 << 12
 	inputs := make([]int, length)
 	for i := 0; i < length; i++ {
-		inputs = append(inputs, rand.Int())
+		v := rand.Intn(100)
+		inputs = append(inputs, v)
 	}
 
-	for _, cpus := range []int{1024} {
-		runtime.GOMAXPROCS(cpus)
-		for name, q := range queues {
-			b.Run(name+"#"+strconv.Itoa(cpus), func(b *testing.B) {
-				b.ResetTimer()
-
-				var c int64
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						i := int(atomic.AddInt64(&c, 1)-1) % length
-						v := inputs[i]
-						if v >= 0 {
-							q.Enqueue(v)
-						} else {
-							q.Dequeue()
-						}
+	for name, q := range queues {
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			var c int64
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					i := int(atomic.AddInt64(&c, 1)-1) % length
+					v := inputs[i]
+					if v >= 50 {
+						q.Enqueue(v)
+					} else {
+						q.Dequeue()
 					}
-				})
+				}
 			})
-		}
+		})
 	}
 }
